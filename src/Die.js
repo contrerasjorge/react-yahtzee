88c9c80d2e184a7ad24c762392ebcf79b0e53a12
Jorge Contreras <jorge.contreras789@gmail.com>
2019-09-07 10:15:42 -0400

    initial commit

---
+++ src/Die.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Die.css';
+
+class Die extends Component {
+  static defaultProps = {
+    numberWords: ['one', 'two', 'three', 'four', 'five', 'six'],
+    val: 6
+  };
+
+  render() {
+    const { numberWords, locked, val, idx, disabled, rolling } = this.props;
+
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if (locked) classes += 'Die-locked';
+    if (rolling) classes += 'Die-rolling';
+    return (
+      <i
+        className={classes}
+        onClick={() => this.props.handleClick(idx)}
+        disabled={disabled}
+      />
+    );
+  }
+}
+
+export default Die;
